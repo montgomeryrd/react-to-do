ae5017383799c05e0148de0a9dc6e495e17c79b1
montgomeryrd <montgomery_rd@yahoo.com>
2020-05-05 20:39:57 -0700

    losing interest in using actions and reducers for this app

--- src/components/dashboard/NavLinks.js
+++ src/components/dashboard/NavLinks.js
@@ -1,14 +1,15 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 function NavLinks(props) {
+
     return (
         <div className="navlinks">
             <div className="navImg" style={ props.data.bgcolor }>
-                <img src={ props.data.imgUrl } alt=""/>
+                <img src={ props.data.imgUrl } onClick={ props.handleView } alt=""/>
             </div>
             <div className="navInfo">
                 <h4>{ props.data.link }</h4>
-                <p>{ props.data.count }</p>
             </div>
         </div>
     )
