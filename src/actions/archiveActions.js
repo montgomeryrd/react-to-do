fa04a3b42ecaf7170ef1e9e0821a881e484f8b0f
montgomeryrd <montgomery_rd@yahoo.com>
2020-05-06 16:13:43 -0700

    Routes are now working. Potential issue with css navigating between pages. Yolo.

--- src/actions/archiveActions.js
+++
@@ -1,9 +0,0 @@
-export const setArchive = archive => ({
-    type: 'SET_ARCHIVE',
-    payload: archive,
-});
-
-export const deleteArchive = archive => ({
-    type: 'DELETE_ARCHIVE',
-    payload: archive,
-});
---
+++ src/components/contentview/Goals.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/goals.css';
+
+function GoalsPage(props){
+    return (
+      <div className="goals-content-view">
+        <h2>Goals Page</h2>
+      </div>
+    ); 
+}
+
+export default GoalsPage;
